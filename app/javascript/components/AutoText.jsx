6e075467b7f26cdfe56cf453c5b94f7ae8f8493f
syahirah <syahirah.suradi@gmail.com>
2019-11-30 21:57:08 +0800

    test search box styling exp for hover colour

--- app/javascript/components/AutoText.jsx
+++ app/javascript/components/AutoText.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../../assets/stylesheets/AutoText.scss'
 
 class AutoText extends React.Component{
     constructor () {
@@ -6,18 +7,50 @@ class AutoText extends React.Component{
         this.items = [
             'Jurong',
             'Changi',
+            'Choa Chu Kang',
             'Ang Mo Kio',
             'Tanjong Pagar',
         ];
+        this.state = {
+            suggestions: [],
+            text: "",
+        }
+    }
+
+    onTextChange = (e) => {
+        const value = e.target.value;
+        let suggestions = [];
+        if (value.length > 0 ){
+            const regex = new RegExp(`^${value}`, 'i');
+            suggestions = this.items.sort().filter(v => regex.test(v));
+        }
+        this.setState(() => ({ suggestions, text: value }) );
+    }
+
+    suggestionSelected (value){
+        this.setState(() => ({
+            text: value,
+            suggestions: [],
+        }))
+    }
+    renderSuggestions () {
+        const {suggestions} = this.state;
+        if (suggestions.length === 0){
+            return null;
+        }
+        return (
+            <ul>
+                {suggestions.map ((item) => <li onClick={() => this.suggestionSelected(item)}>{item}</li>)}
+            </ul>
+        );
     }
 
     render () {
+        const {text} = this.state;
         return (
-            <div>
-                <input onChange={(e) => console.log(e.target.value)} type="text" />
-                    <ul>
-                        {this.items.map ((item) => <li>{item}</li>)}
-                    </ul>
+            <div className="AutoText">
+                <input value={text} onChange={this.onTextChange} type="text" />
+                {this.renderSuggestions ()}
             </div>
         )
     }
